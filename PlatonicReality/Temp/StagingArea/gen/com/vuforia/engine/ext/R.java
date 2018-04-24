f13cb37dd41e0ea112f1e7b742859a1353df92f7
louisreed <mail@louisreed.co.uk>
2018-04-24 23:39:13 +0100

    Responsive fixes

--- PlatonicReality/Temp/StagingArea/gen/com/vuforia/engine/ext/R.java
+++ PlatonicReality/Temp/StagingArea/gen/com/vuforia/engine/ext/R.java
@@ -14,6 +14,7 @@ public final class R {
         public static final int app_banner=0x7f020000;
         public static final int app_icon=0x7f020001;
         public static final int app_icon_round=0x7f020002;
+        public static final int ic_launcher_foreground=0x7f020003;
     }
     public static final class string {
         public static final int app_name=0x7f030000;
